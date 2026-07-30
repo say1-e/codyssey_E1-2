fda9a7d85c010dc933bdad0d773f83badbcc2bee
say1-e <tpdnjs4421@gmail.com>
2026-07-30 15:37:42 +0900

    feat : 프로그램 진입점 구현

---
+++ main.py
@@ -0,0 +1,24 @@
+def show_menu() :
+    print ("=" * 40)
+    print("🎯 나만의 퀴즈 게임 🎯")
+    print ("=" * 40)
+    print("1. 퀴즈 풀기")
+    print("2. 퀴즈 추가")
+    print("3. 퀴즈 목록")
+    print("4. 점수 확인")
+    print("5. 종료")
+
+def main():
+    show_menu()
+
+"""
+__name__ : 파이썬 실행 시 현재 모듈(파이썬 파일)의 이름을 자동으로 담아주는 내장 변수
+    직접 실행된 파일의 __name__ = "__main__"
+    다른 파일에서 import 됐을 때의 __name__ = [해당 모듈 이름]
+"""
+
+# 진입점 Entry Point
+# 이 파일이 직접 실행됐을 때에만 main() 실행 -> import된 다른 코드들까지 멋대로 실행되는 것 방지
+if __name__ == "__main__": 
+    main()
+
