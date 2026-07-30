a06d4e4563567819c2d7cd995d73c2afc10b67f2
say1-e <tpdnjs4421@gmail.com>
2026-07-30 15:51:48 +0900

    feat : 게임 메뉴 출력 메서드 구현

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
 class QuizGame : # class = 설계도
-    def __init__(self) : # 생성자 - 객체 생성 시 자동 실행 메서드
+    def __init__(self) : # 생성자 - 객체 생성 시 자동 실행 메서드 / self라는 변수에 현재 객체 자기 자신에 대한 참조가 들어옴
         self.quizzes = []
         self.best_score =0
 
+    def show_menu(self) : # 일반 함수 - 객체의 행동
+        print ("=" * 40)
+        print("🎯 나만의 퀴즈 게임 🎯")
+        print ("=" * 40)
+        print("1. 퀴즈 풀기")
+        print("2. 퀴즈 추가")
+        print("3. 퀴즈 목록")
+        print("4. 점수 확인")
+        print("5. 종료")
+        print ("=" * 40)
 
-def show_menu() :
-    print ("=" * 40)
-    print("🎯 나만의 퀴즈 게임 🎯")
-    print ("=" * 40)
-    print("1. 퀴즈 풀기")
-    print("2. 퀴즈 추가")
-    print("3. 퀴즈 목록")
-    print("4. 점수 확인")
-    print("5. 종료")
 
 def main():
     game = QuizGame() # 클래스라는 설계도로 객체 생성
-    print("퀴즈 게임 객체 생성")
-    show_menu()
+    game.show_menu()
 
 """
 __name__ : 파이썬 실행 시 현재 모듈(파이썬 파일)의 이름을 자동으로 담아주는 내장 변수
